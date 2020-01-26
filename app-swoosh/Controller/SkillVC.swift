ff048ff95b8c7dbf414712006e21ec54cc0a98f0
Nathaniel Fargo <nathaniel.fargo@gmail.com>
2020-01-25 17:16:30 -0700

    Just a few changes I guess

--- app-swoosh/Controller/SkillVC.swift
+++ app-swoosh/Controller/SkillVC.swift
@@ -18,12 +18,6 @@ class SkillVC: UIViewController {
         print(player.desiredLeague ?? "...")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
     /*
     // MARK: - Navigation
 
